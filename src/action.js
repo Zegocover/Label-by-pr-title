92968a6862032c7ea26dd62e7f6c399c55fddbfe
Gerai2963 <85170301+Gerai2963@users.noreply.github.com>
2021-06-11 17:47:19 +0100

    first attempt to add label bugv7

--- src/action.js
+++ src/action.js
@@ -10,7 +10,7 @@ async function run()
 	const { context = {} } = github;
 	const { pull_request } = context.payload;
 
-	await octokit.issues.createComment({
+	await octokit.rest.issues.createComment({
 		...context.repo,
 		issue_number: pull_request.number,
 		body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'

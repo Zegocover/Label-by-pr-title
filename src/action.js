3051a1982e57604f8fa347313414f98d769735cb
Gerai2963 <85170301+Gerai2963@users.noreply.github.com>
2021-06-11 16:23:05 +0100

    first attempt to add label bug

--- src/action.js
+++ src/action.js
@@ -10,7 +10,17 @@ async function run()
 	const { context = {} } = github;
 	const { pull_request } = context.payload;
 
-	
+//	await octokit.issues.createComment({
+//		...context.repo,
+//		issue_number: pull_request.number,
+//		body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
+//	});	
+
+	await octokit.issues.addLabels({
+		...context.repo,
+		issue_number: pull_request.number,
+		labels: 'bug'
+	});
 	  
 	console.log('Hello, world!');
 	} catch(error)

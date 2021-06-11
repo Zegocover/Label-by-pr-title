4997eb244e28c27404813781a28f0311e3015906
Gerai2963 <85170301+Gerai2963@users.noreply.github.com>
2021-06-11 09:39:44 +0100

    display PR body and title

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const pr_body = github.context.payload.pull_request.body;
+  const pr_title = github.context.payload.pull_request.title; 
+  console.log(`The comments of pull request is: #${pr_body}`);
+  console.log(`The comments of pull request is: #${pr_title}`);
 } catch (error) {
+  console.log('Failed this test');
   core.setFailed(error.message);
+}
+
+function getPrNumber() {
+  const pullRequest = github.context.payload.pull_request;
+  if (!pullRequest) {
+      return undefined;
+  }
+  return pullRequest.number;
 }

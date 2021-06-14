19bd4f58cd0db741be1fc686f144dee7df28569d
Gerai2963 <85170301+Gerai2963@users.noreply.github.com>
2021-06-14 17:09:49 +0100

    attempt 123

--- src/action.js
+++ src/action.js
@@ -10,38 +10,30 @@ async function run()
 	const octokit = github.getOctokit(GITHUB_TOKEN);
 	const { context = {} } = github;
 	const { pull_request } = context.payload;
-	const bugLabel = String["bug"];
+	const allMyLabels = pull_request.labels;
+
+	console.log("PR issue number is: " + github.context.payload.issue_number);
+	console.log("First label name is: " + allMyLabels[0].name);
+
+	await octokit.rest.issues.removeLabel({
+		...context.repo,
+		issue_number: pull_request.number,
+		labels: myLabel[0].name
+	});
+	console.log("Removed first label OK");
 /*
+
+		const readable_Labels = JSON.stringify(allMyLabels,undefined,2);
+	//	console.log("Print all labels: " + readable_Labels);
+	const first_Label = JSON.stringify(allMyLabels[0],undefined,2);
+	console.log("Print first label: " + first_Label);
+
 	await octokit.rest.issues.createComment({
 		...context.repo,
 		issue_number: pull_request.number,
 		body: '3Thank you for submitting a pull request! We will try to review this as soon as we can.'
 	});	
-*/
-	const pr_owner = context.repo.owner;
-	const pr_repo = context.repo;
-	const pr_number = pull_request.number;
-	console.log("comment created successfully");
-	console.log("PR owner is: " + pr_owner);
-	console.log("PR repo is: " + pr_repo);
-	console.log("PR number is: " + pr_number);
-	console.log("PR issue number is: " + github.context.payload.issue_number);
 
-	const myLabel = pull_request.labels;
-		const readable_Labels = JSON.stringify(myLabel,undefined,2);
-	//	console.log("Print all labels: " + readable_Labels);
-
-	const first_Label = JSON.stringify(myLabel[0],undefined,2);
-	console.log("Print first label: " + first_Label);
-	console.log("First label name is: " + myLabel[0].name);
-
-	await octokit.rest.issues.removeLabel({
-		...context.repo,
-		issue_number: pr_number,
-		labels: myLabel[0].name
-	});
-	console.log("Removed first label OK");
-/*
 	await octokit.rest.issues.addLabels({
 		owner: pr_owner,
 		repo: pr_repo,

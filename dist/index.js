21a39f116bf53647e7ba4f3dcf868d195d6a6492
Gerai2963 <85170301+Gerai2963@users.noreply.github.com>
2021-06-16 18:24:57 +0100

    Show data as string7

--- dist/index.js
+++ dist/index.js
@@ -10275,6 +10275,7 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 const yaml = __nccwpck_require__(1917);
+const fs   = __nccwpck_require__(5747);
 
 async function run()
 {
@@ -10295,11 +10296,11 @@ async function run()
 
 	const repo_Labels = await GetLabelsFromRepo(octokit, context);
 	const labelsToAdd = CheckLabelsWithTitle(labels,pr_Title);
-
 	// Testing section
 
 	console.log("Fetch label config file from repo");
-
+	let fileContent = fs.readFile(".github/pr_label_config.yml","utf8");
+	console.log(`Output of FS is: ${content}`);
 	const configurationContent = await GetContent(octokit, context);
 	console.log("Seems to have worked");
 	// loads (hopefully) a `{[label:string]: string | StringOrMatchConfig[]}`, but is `any`:

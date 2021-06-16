09b90d4c7a2d5673572d94f721220d6c04dfff97
Gerai2963 <85170301+Gerai2963@users.noreply.github.com>
2021-06-16 17:20:19 +0100

    Attempt to read a file 4.0

--- src/action.js
+++ src/action.js
@@ -125,7 +125,7 @@ async function fetchContent(octokit, context)
 	//const response = await octokit.rest.repos.getContents({
 	const response = await octokit.rest.repos.getContent({
 	  ...context,
-	  path: "../.github/pr_label_config.yml",
+	  path: ".github/pr_label_config.yml",
 	});
       
 	//return Buffer.from(response.data.content, response.data.encoding).toString();

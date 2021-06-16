8475175d5f9f6cfa01a856c9c4500cec54ff3bb4
Gerai2963 <85170301+Gerai2963@users.noreply.github.com>
2021-06-16 17:17:21 +0100

    Attempt to read a file 3.9

--- dist/index.js
+++ dist/index.js
@@ -10400,7 +10400,6 @@ async function fetchContent(octokit, context)
 	const response = await octokit.rest.repos.getContent({
 	  ...context,
 	  path: "../.github/pr_label_config.yml",
-	  ref: "Read-Labels-from-File"
 	});
       
 	//return Buffer.from(response.data.content, response.data.encoding).toString();

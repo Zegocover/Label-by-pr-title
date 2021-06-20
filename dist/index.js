dbd9f89263e4b65795c047e7e49c8ef13ddc7df0
Gerai2963 <85170301+Gerai2963@users.noreply.github.com>
2021-06-20 13:30:45 +0100

    another attempt using YAML decoding 1

--- dist/index.js
+++ dist/index.js
@@ -10302,7 +10302,7 @@ async function run()
 	console.log("Get label config file from repo");
 	//Get the file content
 	const configurationContent = await GetContent(octokit, context);
-	const configObject = yaml.load(configurationContent);
+	const cencodedFileContent = yaml.load(configurationContent);
 	//let encodedFileContent = new Buffer(configObject, 'base64');
 
 	console.log(`Hopefully decoded ${encodedFileContent.toString('utf8')}`);

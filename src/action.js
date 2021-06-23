0f22c65e836a71f738d3935dd882d724b7877ae6
Gerai2963 <85170301+Gerai2963@users.noreply.github.com>
2021-06-23 17:35:09 +0100

    removed DefineLabels2 function

--- src/action.js
+++ src/action.js
@@ -254,27 +254,4 @@ function Arr_Match(arrBase, strMatch)
 	return false;
 }
 
-/* Define the array of labels and their matching string as: array[array[]]
-*  [['labelname1','matchword1','matchword2'], ['labelname2','matchword3','matchword4']]
-*  return: array[array[]]
-*/
-function DefineLabelMatches2()
-{
-	//Label associations
-	const bugFixLabel = ['bugfix','bugfix'];
-	const featLabel = ['feat','feat']
-	const hotFixLabel = ['hotfix','hotfix']
-	const refactorLabel = ['refactor','refactor']
-	const choreLabel = ['chore','chore']
-
-	const labels = [];
-	labels.push(bugFixLabel);
-	labels.push(featLabel);
-	labels.push(hotFixLabel);
-	labels.push(refactorLabel);
-	labels.push(choreLabel);
-
-	return labels;
-}
-
 run();
